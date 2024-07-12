35002723a63ee20838b2a81b61b573510da23a3c
Leonid Pilyugin <l.pilyugin04@gmail.com>
2024-07-12 13:50:22 +0300

    Colorful logger output

--- src/spmi/utils/logger.py
+++ src/spmi/utils/logger.py
@@ -5,6 +5,28 @@ import logging
 
 class Logger:
     """Provides logging methods."""
+    class DefaultFormatter(logging.Formatter):
+
+        grey = "\x1b[38;20m"
+        green = "\x1b[32;20m"
+        yellow = "\x1b[33;20m"
+        red = "\x1b[31;20m"
+        bold_red = "\x1b[31;1m"
+        reset = "\x1b[0m"
+        format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+        FORMATS = {
+            logging.DEBUG: grey + format + reset,
+            logging.INFO: green + format + reset,
+            logging.WARNING: yellow + format + reset,
+            logging.ERROR: red + format + reset,
+            logging.CRITICAL: bold_red + format + reset
+        }
+
+        def format(self, record):
+            log_fmt = self.FORMATS.get(record.levelno)
+            formatter = logging.Formatter(log_fmt)
+            return formatter.format(record)
 
     @staticmethod
     def basic_config(loglevel="INFO"):
@@ -25,7 +47,12 @@ class Logger:
         Args:
             name (:obj:`str`): Logger name.
         """
-        self.logger = logging.getLogger(name)
+        self._logger = logging.getLogger(name)
+        self._logger.handlers.clear()
+        ch = logging.StreamHandler()
+        ch.setFormatter(Logger.DefaultFormatter())
+        self._logger.addHandler(ch)
+        self._logger.propagate = False
 
     def debug(self, msg):
         """Debug a message.
@@ -33,7 +60,7 @@ class Logger:
         Args:
             msg (:obj:`str`): message to show.
         """
-        self.logger.debug(msg)
+        self._logger.debug(msg)
 
     def info(self, msg):
         """Info a message.
@@ -41,7 +68,7 @@ class Logger:
         Args:
             msg (:obj:`str`): message to show.
         """
-        self.logger.info(msg)
+        self._logger.info(msg)
 
     def warning(self, msg):
         """Warning a message.
@@ -49,7 +76,7 @@ class Logger:
         Args:
             msg (:obj:`str`): message to show.
         """
-        self.logger.warning(msg)
+        self._logger.warning(msg)
 
     def error(self, msg):
         """Error a message.
@@ -57,7 +84,7 @@ class Logger:
         Args:
             msg (:obj:`str`): message to show.
         """
-        self.logger.error(msg)
+        self._logger.error(msg)
 
     def critical(self, msg):
         """Critical a message.
@@ -65,4 +92,4 @@ class Logger:
         Args:
             msg (:obj:`str`): message to show.
         """
-        self.logger.critical(msg)
+        self._logger.critical(msg)
